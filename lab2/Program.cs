b043a6d2f0aa31c4bff3c73dbf8a3ae8275bc103
Methsavi <hmmsavindya.students.nsbm.ac.lk>
2024-01-31 13:55:46 +0530

    Add project files.

---
+++ lab2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of terms (N) for Fibonacci series: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Fibonacci Series:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(Fibonacci(i) + " ");
+            }
+
+            Console.ReadLine();
+        }
+
+        static int Fibonacci(int n) 
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+            else
+            {
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+            }
+        }
+    }
+}
